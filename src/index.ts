94470a8af8818fd315d3dda110649325d956d126
nadakhattab98 <nadaKhattab98@gmail.com>
2020-10-19 09:09:07 +0200

    Add connection to DB

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.get("/",(req,res)=>{
-    console.log("HEYY");
-    res.send("HELLO PEOPLE");
-})
+app.get("/", (req, res) => {
+  console.log("HEYY");
+  res.send("HELLO PEOPLE");
+});
 
-app.listen(3000,()=>console.log("SERVER IS UP AND RUNNING"));
+mongoose.connect(
+  process.env.DB_CONNECT || "",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  () => console.log("Connected to database")
+);
+
+app.listen(3000, () => console.log("SERVER IS UP AND RUNNING"));
